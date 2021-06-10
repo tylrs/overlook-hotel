fc23eb81e69aaa1bada89bf8868a6a077afa36c0
Taylor Galloway <gallowaytaylor@outlook.com>
2021-06-09 19:16:12 -0500

    Add more sample data

---
+++ test/sample-data.js
@@ -0,0 +1,88 @@
+export const customerData = [
+  {
+    "id": 1,
+    "name": "Leatha Ullrich"
+  },
+  {
+    "id": 2,
+    "name": "Rocio Schuster"
+  },
+  {
+    "id": 3,
+    "name": "Kelvin Schiller"
+  },
+  {
+    "id": 4,
+    "name": "Kennedi Emard"
+  },
+  {
+    "id": 5,
+    "name": "Rhiannon Little"
+  },
+];
+
+export const bookingsData = [
+  {
+    id: '5fwrgu4i7k55hl6t8',
+    userID: 1,
+    date: '2020/02/05',
+    roomNumber: 12,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6tl',
+    userID: 1,
+    date: '2020/01/10',
+    roomNumber: 8,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6tv',
+    userID: 1,
+    date: '2020/01/19',
+    roomNumber: 21,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6u0',
+    userID: 1,
+    date: '2020/01/08',
+    roomNumber: 5,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6uf',
+    userID: 2,
+    date: '2020/01/09',
+    roomNumber: 18,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6uy',
+    userID: 2,
+    date: '2020/01/24',
+    roomNumber: 19,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6v3',
+    userID: 3,
+    date: '2020/02/07',
+    roomNumber: 23,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6vc',
+    userID: 4,
+    date: '2020/01/18',
+    roomNumber: 18,
+    roomServiceCharges: []
+  },
+  {
+    id: '5fwrgu4i7k55hl6vn',
+    userID: 5,
+    date: '2020/02/20',
+    roomNumber: 1,
+    roomServiceCharges: []
+  }
+];
