b964cee41c6270ba85bc026fb2c2d8bba99679a4
Taylor Galloway <gallowaytaylor@outlook.com>
2021-06-12 14:36:05 -0500

    Create renderBookingsCards method

---
+++ src/domUpdates.js
@@ -0,0 +1,19 @@
+let domUpdates = {
+  renderBookingsCards(element, currentCustomer, currentDate, type) {
+    element.innerHTML = '';
+    let bookings = currentCustomer.returnBookings(type, currentDate)
+    bookings.forEach(booking => {
+      element.innerHTML +=
+      `
+        <article class="booking-card">
+          <h5>2020/02/04</h4>
+          <p>Residential Suite</p>
+          <p>Room Number 5</p>
+          <p>$358.40 / Night</p>
+        </article>
+      `
+    })
+  }
+}
+
+export default domUpdates;
