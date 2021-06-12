ea4d2cd02fbc90ca69d585ad6c8f371f44d80803
Taylor Galloway <gallowaytaylor@outlook.com>
2021-06-12 10:37:35 -0500

    Instantiate classes and import sample data

--- src/scripts.js
+++ src/scripts.js
@@ -6,5 +6,15 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import {customerData, roomsData, bookingsData, allRoomsBooked} from '../test/sample-data.js'
+import Customer from './classes/Customer.js'
+import Hotel from './classes/Hotel.js'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let currentCustomer, hotel, currentDate;
+
+customer = new Customer(customerData[0]);
+currentDate = '2020/02/03';
+hotel = new Hotel(bookingsData, roomsData);
+// instantiate customer, hotel, currentDate
+//querySelectors
+//populate upcoming and past and total spent
